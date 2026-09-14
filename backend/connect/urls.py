40ab8cc013919d0e7cb9167d0b83f6425118f897
coll-ins <mainacollins184@gmail.com>
2026-09-15 00:54:53 +0300

    Combine root view and app routes in urls.py

--- backend/connect/urls.py
+++ backend/connect/urls.py
@@ -1,8 +1,12 @@
-# Main URL router placeholder
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to Connect API!")
 
 urlpatterns = [
+    path('', home_view),  # Handles the root URL so you don't get a 404
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/companies/', include('companies.urls')),
