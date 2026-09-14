dfea837c4cd99e981a5bb2e0dfb1f25eed7707f7
coll-ins <mainacollins184@gmail.com>
2026-09-15 02:07:24 +0300

    Replace root URL with TemplateView to serve frontend

--- backend/connect/urls.py
+++ backend/connect/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("Welcome to Connect API!")
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', home_view),  # Handles the root URL so you don't get a 404
+    path('', TemplateView.as_view(template_name='index.html')),  # Serves your frontend HTML template
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/companies/', include('companies.urls')),
